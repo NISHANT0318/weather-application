c5c839cb61c32d2d63264785a25d9da721a1d417
Nishant <Nishantsatone1997@gmail.com>
2024-06-24 08:50:57 +0530

    Completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import WeatherApp from './components/WeatherApp'
+import './components/WeatherApp.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+function App() {
+
+  return (
+    <>
+      <WeatherApp/>
+    </>
+  )
+}
+
+export default App
